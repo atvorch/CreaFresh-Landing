c7431876d1d8fa353dcf7b6af768cf497dab86fa
andrey <ashahverdyan@toidev.com>
2018-05-22 14:37:02 +0300

    Fixed missing dependencies in package.json;

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,48 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var autiprefixer = require('gulp-autoprefixer');
 var minifyjs = require('gulp-js-minify');  
-let sourcemaps = require('gulp-sourcemaps'); 
+var sourcemaps = require('gulp-sourcemaps'); 
 var browserSync = require('browser-sync');
-
+var jimp = require('gulp-jimp');
+var gulpSequence = require('gulp-sequence');
 
 const server = browserSync.create();
 
 const path = {
     src: {
+		images: 'assets/images/*.jpg',
 		scss: 'stylesheets/*.scss',
 		js: 'js/*.js'
 	},
 	build: {
+		images: 'assets/images/placeholders/',
 		scss: './',
 		js: './'
 	},
 	watch: {
+		images: 'assets/images/*.jpg',
 		html: 'index.html',
 		css: 'stylesheets/*.css',
 		scss: 'stylesheets/*.scss',
 		js: 'js/*.js',
 	}
 };
+
+//Compress and blur images for images placeholder for images lazy loading
+gulp.task('build:img', function () {  
+	return gulp.src(path.src.images)
+	.pipe(jimp({
+		'-placeholder' : {
+			quality: 50,
+			gaussian: 2,
+			blur: 2,
+			type: 'jpg'
+		}
+	}))
+	.pipe(gulp.dest(path.build.images));
+});
+
+//minify js
 gulp.task('build:js', function () {  
 	return gulp.src(path.src.js)
 	.pipe(sourcemaps.init())
@@ -32,6 +52,8 @@ gulp.task('build:js', function () {
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest(path.build.js));
 });
+
+//build sass
 gulp.task('build:sass', function () {  
 	return gulp.src(path.src.scss)
 	.pipe(sass())
@@ -42,17 +64,21 @@ gulp.task('build:sass', function () {
 	.pipe(gulp.dest(path.build.scss));
 });
 
+//run browsersync
 gulp.task('browser-sync', function() {  
-    browserSync.init([path.watch.scss,path.watch.css, path.watch.html,  path.watch.js], {
+    browserSync.init([path.watch.scss, path.watch.img, path.watch.html, path.watch.js], {
         server: {
             baseDir: "./"
         }
     });
 });
 
-gulp.task('build', ['build:sass','build:js', 'browser-sync'], function () {  
+gulp.task('watch', function () {
+	gulp.watch(path.watch.img, ['build:img']);
 	gulp.watch(path.watch.scss, ['build:sass']);
 	gulp.watch(path.watch.js, ['build:js']);
 });
+
+gulp.task('build', gulpSequence('build:img', 'build:sass', 'build:js', 'browser-sync', 'watch'));
 
 
